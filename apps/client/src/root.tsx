08fdc91d7f4b6b9d0702f5e7a186ac25c2afa7ae
Pavaresorn Malai <61514399+Varkaria@users.noreply.github.com>
2022-11-19 16:03:40 +0700

    feat: fix zoom

--- apps/client/src/root.tsx
+++ apps/client/src/root.tsx
@@ -20,7 +20,7 @@ export default function Root() {
       <Head>
         <Title>Remote Control</Title>
         <Meta charset="utf-8" />
-        <Meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <Meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0"/>
       </Head>
       <Body>
         <Suspense>
